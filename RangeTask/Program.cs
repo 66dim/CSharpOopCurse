cee67ca8178ef2ea244b1cd16d6d536e89146d2c
Dmitry <vasilevde@outlook.com>
2023-07-14 02:07:51 +0700

    Add code in main and range.

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите число - начало диапазона: ");
+            double start = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число - конец диапазона: ");
+            double end = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(start, end);
+
+            double length = range.GetLength();
+            Console.WriteLine("Длина диапазона равна: " + length);
+
+            Console.WriteLine("Введите число для проверки принадлежности диапазону:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Число {0} принадлежит диапазону", number);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} не принадлежит диапазону", number);
+            }
+
+            range.Print();
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RangeTask
+﻿namespace RangeTask
 {
-    internal class Class1
+    public class Range
     {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Первое число диапазона: {0} Последнее число диапазона: {1}", From, To);
+        }
     }
 }
